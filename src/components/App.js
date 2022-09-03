daa49723f815db9b3619697ec7befc873e711f88
Carlos Calderon <cdcalderon@gmail.com>
2022-09-03 13:50:20 -0500

    prepare initial project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello World!</h1>
+        <p>This is some placeholder text</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
