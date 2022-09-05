de24f257c1958f22d46c39538d8b9db9f5d5d5a5
Carlos Calderon <cdcalderon@gmail.com>
2022-09-05 17:38:56 -0500

    implement deposit and withdraw ui selection handlers

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import dapp from "../assets/dapp.svg";
+import eth from "../assets/eth.svg";
+
 import { loadBalances, transferTokens } from "../store/interactions";
 
 const Balance = () => {
   const [token1TransferAmount, setToken1TransferAmount] = useState(0);
+  const [token2TransferAmount, setToken2TransferAmount] = useState(0);
+  const [isDeposit, setIsDeposit] = useState(true);
 
   const dispatch = useDispatch();
 
@@ -22,9 +26,14 @@ const Balance = () => {
   const symbols = useSelector((state) => state.tokens.symbols);
   const tokenBalances = useSelector((state) => state.tokens.balances);
 
+  const depositRef = useRef(null);
+  const withdrawRef = useRef(null);
+
   const amountHandler = (e, token) => {
     if (token.address === tokens[0].address) {
       setToken1TransferAmount(e.target.value);
+    } else {
+      setToken2TransferAmount(e.target.value);
     }
   };
 
@@ -41,6 +50,28 @@ const Balance = () => {
         dispatch
       );
       setToken1TransferAmount(0);
+    } else {
+      transferTokens(
+        provider,
+        exchange,
+        "Deposit",
+        token,
+        token2TransferAmount,
+        dispatch
+      );
+      setToken2TransferAmount(0);
+    }
+  };
+
+  const tabHandler = (e) => {
+    if (e.target.className !== depositRef.current.className) {
+      e.target.className = "tab tab--active";
+      depositRef.current.className = "tab";
+      setIsDeposit(false);
+    } else {
+      e.target.className = "tab tab--active";
+      withdrawRef.current.className = "tab";
+      setIsDeposit(true);
     }
   };
 
@@ -56,8 +87,16 @@ const Balance = () => {
       <div className="component__header flex-between">
         <h2>Balance</h2>
         <div className="tabs">
-          <button className="tab tab--active">Deposit</button>
-          <button className="tab">Withdraw</button>
+          <button
+            onClick={tabHandler}
+            ref={depositRef}
+            className="tab tab--active"
+          >
+            Deposit
+          </button>
+          <button onClick={tabHandler} ref={withdrawRef} className="tab">
+            Withdraw
+          </button>
         </div>
       </div>
 
@@ -94,7 +133,7 @@ const Balance = () => {
           />
 
           <button className="button" type="submit">
-            <span>Deposit</span>
+            {isDeposit ? <span>Deposit</span> : <span>Withdraw</span>}
           </button>
         </form>
       </div>
@@ -104,14 +143,37 @@ const Balance = () => {
       {/* Deposit/Withdraw Component 2 (mETH) */}
 
       <div className="exchange__transfers--form">
-        <div className="flex-between"></div>
+        <div className="flex-between">
+          <p>
+            <small>Token</small>
+            <br />
+            <img src={eth} alt="Token Logo" />
+            {symbols && symbols[1]}
+          </p>
+          <p>
+            <small>Wallet</small>
+            <br />
+            {tokenBalances && tokenBalances[1]}
+          </p>
+          <p>
+            <small>Exchange</small>
+            <br />
+            {exchangeBalances && exchangeBalances[1]}
+          </p>
+        </div>
 
-        <form>
+        <form onSubmit={(e) => depositHandler(e, tokens[1])}>
           <label htmlFor="token1"></label>
-          <input type="text" id="token1" placeholder="0.0000" />
+          <input
+            type="text"
+            id="token1"
+            placeholder="0.0000"
+            value={token2TransferAmount === 0 ? "" : token2TransferAmount}
+            onChange={(e) => amountHandler(e, tokens[1])}
+          />
 
           <button className="button" type="submit">
-            <span></span>
+            {isDeposit ? <span>Deposit</span> : <span>Withdraw</span>}
           </button>
         </form>
       </div>
